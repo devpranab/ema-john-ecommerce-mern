edfbb17325b1b5193e18d5a04a518ad5b8bdceb4
pranav s <dev.pranabsk@gmail.com>
2022-09-06 16:36:42 +0530

    17 Retrieve data from local storage, set cart from saved state

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Review.css';
+import {getDatabaseCart} from '../../utilities/databaseManager';
+import fakeData from '../../fakeData';
 
 const Review = () => {
+    const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+        //cart
+        const savedCart = getDatabaseCart();
+         //console.log(savedCart);
+        const productKeys = Object.keys(savedCart);
+        const cartProducts = productKeys.map(key => {
+            const product = fakeData.find(product => product.key === key);
+            product.quantity = savedCart[key];
+            return product;
+        });
+        //console.log(cartProducts);
+        setCart(cartProducts);
+    },[]);
+
     return (
         <div>
             Review
+           <h2>Cart Items: {cart.length}</h2>
         </div>
     );
 };
